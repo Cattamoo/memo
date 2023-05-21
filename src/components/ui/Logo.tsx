905b48549c0e565d93c98fe9692db91006b92269
Cattamoo <ouob77@kakao.com>
2023-05-22 01:31:00 +0900

    로고

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -3,7 +3,7 @@ import {FaPencilAlt} from "react-icons/fa";
 
 export default function Logo() {
 	return (
-		<div className="flex items-center font-bold text-xl">
+		<div className="flex items-center font-bold text-5xl">
 			<span className="text-red-500">M</span>
 			<span className="text-amber-500">E</span>
 			<span className="text-green-500">M</span>
