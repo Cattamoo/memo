7b12a7c39a2ad4d5a5860d99795f4ea1040558c2
Cattamoo <ouob77@kakao.com>
2023-05-22 01:46:40 +0900

    Header, Logo 색상 수정

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -8,10 +8,8 @@ export default function Header() {
 		<header className="flex flex-col gap-4 items-center bg-white/50 shadow-lg pt-4 pb-2 mb-2">
 			<Link to="/"><Logo /></Link>
 			<div>
-				<Link to="/add">
-					<span className="flex items-center gap-1.5">
-						<FaPlusSquare /> 메모 추가
-					</span>
+				<Link to="/add" className="flex items-center gap-1.5 text-zinc-500 hover:text-zinc-300">
+					<FaPlusSquare /> 메모 추가
 				</Link>
 			</div>
 		</header>
--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -3,12 +3,12 @@ import {FaPencilAlt} from "react-icons/fa";
 
 export default function Logo() {
 	return (
-		<div className="flex items-center font-bold text-5xl">
-			<span className="text-red-500">M</span>
-			<span className="text-amber-500">E</span>
-			<span className="text-green-500">M</span>
-			<span className="text-indigo-500">O</span>
-			<span className="text-violet-500"><FaPencilAlt /></span>
+		<div className="group flex items-center font-bold text-5xl">
+			<span className="text-red-500 group-hover:text-red-300">M</span>
+			<span className="text-amber-500 group-hover:text-amber-300">E</span>
+			<span className="text-green-500 group-hover:text-green-300">M</span>
+			<span className="text-indigo-500 group-hover:text-indigo-300">O</span>
+			<span className="text-violet-500 group-hover:text-violet-300"><FaPencilAlt /></span>
 		</div>
 	);
 }
