275884019cbdc1aa1a37454f7cdb93952d214694
Cattamoo <ouob77@kakao.com>
2023-05-22 13:08:32 +0900

    width 수정

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -5,7 +5,7 @@ import {Link} from "react-router-dom";
 
 export default function Header() {
 	return (
-		<header className="flex flex-col gap-4 items-center bg-white/50 shadow-lg pt-4 pb-2 mb-2">
+		<header className="w-full flex flex-col gap-4 items-center bg-white/50 shadow-lg pt-4 pb-2 mb-2">
 			<Link to="/"><Logo /></Link>
 			<div>
 				<Link to="/add" className="flex items-center gap-1.5 text-zinc-500 hover:text-zinc-300">
