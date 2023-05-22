e1bf9de628f9155ce86917a93ea447c39b0763f3
Cattamoo <ouob77@kakao.com>
2023-05-23 00:12:14 +0900

    EditForm 수정

--- src/components/EditForm.tsx
+++ src/components/EditForm.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Memo} from "../types/memoTypes";
 import {createMemo, updateMemo} from "../store/reducers/memoReducer";
 import {useDispatch} from "react-redux";
@@ -10,8 +10,8 @@ type Props = {
 export default function EditForm({ memo }: Props) {
 	const dispatch = useDispatch();
 	const { id, title, content } = memo || {};
-	const [titleText, setTitleText] = useState(title);
-	const [contentText, setContentText] = useState(content);
+	const [titleText, setTitleText] = useState<string>();
+	const [contentText, setContentText] = useState<string>();
 
 	const handleSubmit = (e: React.FormEvent) => {
 		e.preventDefault();
@@ -22,11 +22,29 @@ export default function EditForm({ memo }: Props) {
 		}
 	}
 
+	useEffect(() => {
+		setTitleText(title || '');
+		setContentText(content || '')
+	}, [title, content])
+
 	return (
 		<form className="w-full flex flex-col" onSubmit={handleSubmit}>
-			<input type="text" name="title" placeholder="제목" value={titleText} onChange={({target}) => setTitleText(target.value)} />
-			<textarea name="content" placeholder="내용" value={contentText} onChange={({target}) => setContentText(target.value)} />
-			<button>저장</button>
+			<input
+				className="outline-none"
+				type="text"
+				name="title"
+				placeholder="제목"
+				value={titleText}
+				onChange={({target}) => setTitleText(target.value)}
+			/>
+			<textarea
+				className="outline-none resize-none"
+				name="content"
+				placeholder="내용"
+				value={contentText}
+				onChange={({target}) => setContentText(target.value)}
+			/>
+			<button className="disabled:bg-zinc-50 disabled:text-zinc-300" disabled={titleText === '' || contentText === ''}>저장</button>
 		</form>
 	);
 }
